629a5300cefc4c100978c9646b207d2f2501ae44
Syond <syond94@gmail.com>
2020-08-04 19:19:16 -0300

    Express configured

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,9 @@
-console.log("Hellow")
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send("E ai :)")
+});
+
+app.listen(process.env.PORT || 3000);
