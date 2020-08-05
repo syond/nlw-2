70186401cd6574c5471bc1df647987e96a722756
Syond <syond94@gmail.com>
2020-08-04 21:24:22 -0300

    Knex configurated

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send("E ai :)")
+app.get("/", (request, response) => {
+  response.send("E ai :)");
 });
 
 app.listen(process.env.PORT || 3000);
