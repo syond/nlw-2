544cfca1a604b19286d46a61d94c557af27ae8b4
Syond <syond94@gmail.com>
2020-08-05 14:19:00 -0300

    get /classes route finished. Added Connections routes.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,14 @@
 import express from "express";
 
 import ClassesController from "./controllers/ClassesController";
+import ConnectionsController from "./controllers/ConnectionsController";
 
 const routes = express.Router();
 
+routes.get("/classes", ClassesController.index);
 routes.post("/classes", ClassesController.store);
+
+routes.get("/connections", ConnectionsController.index);
+routes.post("/connections", ConnectionsController.store);
 
 export default routes;
