0c3d5ce22ae00edb4f99a34d6f4a52fc3f2e462f
Syond <syond94@gmail.com>
2020-08-10 15:08:18 -0300

    .env configured

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: process.env.REACT_APP_API_URL,
 })
 
 export default api;
